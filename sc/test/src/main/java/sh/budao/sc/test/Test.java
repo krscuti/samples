3baa030851af8b27091fb7d389efb05c48e82b8f
kr <krsctst@gmail.com>
2020-03-02 15:41:39 +0100

    init sc

---
+++ sc/test/src/main/java/sh/budao/sc/test/Test.java
@@ -0,0 +1,9 @@
+package sh.budao.sc.test;
+
+public class Test {
+
+    public static void main(String...  args){
+        System.out.println(97%5);
+    }
+
+}
---
+++ sc/test/src/test/java/sh/budao/sc/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.budao.sc.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
