fd9ca7c061fcb4c5d4413f9bf22ac708fa174c75
root <root@vmi330969.contaboserver.net>
2020-03-25 01:25:55 +0800

    add toosimple

---
+++ toosimple/gateway/src/main/java/io/toosimple/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package io.toosimple.gateway;
+
+import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//@SpringBootApplication
+@SpringCloudApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
---
+++ toosimple/gateway/src/main/java/io/toosimple/gateway/config/RouteConfig.java
@@ -0,0 +1,8 @@
+package io.toosimple.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+}
