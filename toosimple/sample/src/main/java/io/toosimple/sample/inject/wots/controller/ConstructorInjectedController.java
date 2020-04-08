552e4cee020c15800fecee456553d8bc75b252d9
kr <kr@vmi330969.contaboserver.net>
2020-04-08 09:59:56 +0800

    add constrct

--- toosimple/sample/src/main/java/io/toosimple/sample/inject/wots/controller/ConstructorInjectedController.java
+++ toosimple/sample/src/main/java/io/toosimple/sample/inject/wots/controller/ConstructorInjectedController.java
@@ -1,8 +1,20 @@
 package io.toosimple.sample.inject.wots.controller;
 
+import io.toosimple.sample.inject.wots.services.GreetingService;
+
 /**
  * sample of ConstructorInject
  */
 public class ConstructorInjectedController {
+
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService){
+        this.greetingService = greetingService;
+    }
+
+    public String getGreetingService() {
+        return greetingService.sayGreeting();
+    }
 
 }
