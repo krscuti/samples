6e8c6d09b99d8abf25d70e840752b338ca772c72
Robbin Golden <krscuti@gmail.com>
2020-08-08 22:36:02 +0800

    commit demo

--- spring-cloud-gateway-demo/auth/src/main/java/io/tosimple/demo/auth/controller/ExampleController.java
+++ spring-cloud-gateway-demo/auth/src/main/java/io/tosimple/demo/auth/controller/ExampleController.java
@@ -1,16 +1,16 @@
-package io.tosimple.demo.gateway.controller;
+package io.tosimple.demo.auth.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/gateway")
+@RequestMapping("/auth")
 public class ExampleController {
 
     @RequestMapping(name="/hello", value = "/hello", method = RequestMethod.GET)
     public String reqGate(){
-        return "hello from gateway！";
+        return "hello from auth service！";
     }
 
 }
